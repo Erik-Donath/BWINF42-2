6c11a7cd54e06fb33fde32813804bfb6d22ebcf4
C42xe2 <erikdonath2006@gmail.com>
2023-12-27 13:45:14 +0100

    Reading file
    Read the file and extract Data if posible.

--- Stilvolle-Paeckchen/src/Main.cpp
+++ Stilvolle-Paeckchen/src/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <vector>
 
 #include "Solution.h"
@@ -8,27 +9,90 @@
 
 i32 main(i32 argc, char* argv[]) {
     std::setlocale(LC_ALL, "de");
+
     std::ifstream file;
     std::string filePath;
 
-    // Parameter Datei wird gelesen
+    // Parameter Datei wird geöfnet
     if (argc >= 2) {
         filePath = argv[1];
-        file.open(argv[1], std::ios::in);
+        file.open(filePath.c_str(), std::ios::in);
         std::cout << "Die Datei, die als Parameter übergeben wurde, "
                   << (file.is_open() ? "wird verwendet!" : "konnte nicht gelesen werden!")
                   << std::endl;
     }
 
-    // Nutzer wird nach Datei gefragt
+    // Nutzer wird nach Datei Pfad gefragt
     while (!file.is_open()) {
         std::cout << "Gib den Pfad zur Datei an: ";
         std::cin >> filePath;
         file.open(filePath.c_str(), std::ios::in);
     }
 
+    if (!file.good()) {
+        file.close();
+        std::cout << "Datei konnte nicht gelesen werden!" << std::endl;
+        return 1;
+    }
+
+    // Read file
+    std::string line;
+    std::stringstream stream;
+
+    u32 sortenAnzahl = 0, stillAnzahl = 0;
+    if (std::getline(file, line)) {
+        stream.clear();
+        stream << line;
+
+        stream >> sortenAnzahl;
+        stream >> stillAnzahl;
+    }
+
+    std::vector<Kombination> kombinationen;
+    while (std::getline(file, line) && !line.empty()) {
+        stream.clear();
+        stream << line;
+
+        Kombination k;
+        stream >> k.x;
+        stream >> k.y;
+        kombinationen.push_back(k);
+    }
+
+    std::vector<Sorte> sorten;
+    while (std::getline(file, line)) {
+        stream.clear();
+        stream << line;
+
+        Sorte s;
+        stream >> s.i;
+        stream >> s.j;
+        stream >> s.n;
+        sorten.push_back(s);
+    }
     file.close();
-    std::cout << "Die Datei ist: " << filePath << std::endl;
+
+    // Solve
+    if (kombinationen.size() == 0 || sorten.size() == 0) {
+        std::cout << "Datei ist fehlerhaft oder Kombinationen/Sorten sind falsch angegeben worden." << std::endl;
+        return 1;
+    }
+
+    std::vector<Packet> packete = Solve(sortenAnzahl, stillAnzahl, kombinationen, sorten);
+
+    // Output
+    std::cout << "Sorten Anzahl: " << sortenAnzahl << std::endl;
+    std::cout << "Still Anzahl: "  << stillAnzahl  << std::endl;
+
+    std::cout << "Kombinationen: " << std::endl;
+    for (Kombination k : kombinationen) {
+        std::cout << "  " << k.x << " <> " << k.y << std::endl;
+    }
+
+    std::cout << "Sorten: " << std::endl;
+    for (Sorte s : sorten) {
+        std::cout << "  " << s.i << " + " << s.j << " => " << s.n << std::endl;
+    }
 
     return 0;
 }
